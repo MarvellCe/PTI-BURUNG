ba3e06549466fc8ff561bacb5b414784b2b5bd0f
Marvell <marvellwij@gmail.com>
2024-05-18 22:12:51 +0700

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login.jsx';
 import Character from './pages/Character.jsx';
 import AboutUs from "./pages/AboutUs.jsx";
@@ -26,7 +26,7 @@ import MuseumPalembang from './pages/kota/palembang/museumPalembang.jsx';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/PTI-BURUNG/" element={<Login />} />
         <Route path="/PTI-BURUNG/character" element={<Character />} />
@@ -52,7 +52,7 @@ const App = () => {
         <Route path="/PTI-BURUNG/restoranpalembang" element={<RestoranPalembang />} />
         <Route path="/PTI-BURUNG/museumpalembang" element={<MuseumPalembang />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
