b8781b672a60d2f42f1b1a3a3fd0f42d192fc098
Marvell <marvellwij@gmail.com>
2024-05-18 21:05:16 +0700

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import '../index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes Routes={route} />
   </React.StrictMode>,
   document.getElementById('root')
 );
